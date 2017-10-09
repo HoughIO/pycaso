7399cd60d692c35cd790114cb8c83e29289a59b8
Graham Hough <graham@hough.io>
2017-10-09 14:36:00 -0400

    starting to get an idea

--- diff.py
+++ diff.py
@@ -1,10 +1,12 @@
 import sys
 from itertools import izip
 from PIL import Image
+import numpy as np
+import cv2 as cv
 
-def pxcompare(image1, image2):
-  i1 = Image.open(image1)
-  i2 = Image.open(image2)
+def pxcompare(sourceimg, outputimg):
+  i1 = Image.open(sourceimg)
+  i2 = Image.open(outputimg)
   assert i1.mode == i2.mode, "Different kinds of images."
   assert i1.size == i2.size, "Different sizes."
   pairs = izip(i1.getdata(), i2.getdata())
@@ -14,7 +16,19 @@ def pxcompare(image1, image2):
   else:
       dif = sum(abs(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2))
   ncomponents = i1.size[0] * i1.size[1] * 3
-  print "Difference (percentage):", (dif / 255.0 * 100) / ncomponents
+  print((dif / 256.0 * 100) / ncomponents)
 
+def drawcanvas(sourceimg):
+  i1 = Image.open(sourceimg)
+  print(i1.size)
+  output = np.zeros((i1.size[0],i1.size[1],3), np.uint8)
+  cv.imshow('output', output)
+  output.save("test.jpg")
+  cv.waitKey(0)
+
+def saveimg(outputname):
+  output.save(outputname)
 
 pxcompare(sys.argv[1], sys.argv[2])
+drawcanvas(sys.argv[1])
+
