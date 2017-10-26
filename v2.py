7dc5b1e170935b7db70ad176c47a7c96a5640429
Graham Hough <graham@hough.io>
2017-10-26 18:41:42 -0400

    after a week, picked this up

--- v2.py
+++ v2.py
@@ -16,6 +16,7 @@ class SourceImg():
         self.sizex = self.imgFile.size[0]
         self.sizey =self.imgFile.size[1]
         self.data = self.imgFile.getdata()
+        self.bands = self.imgFile.getbands()
 
 class Canvas():
     """ A blank, black canvas """
@@ -26,6 +27,7 @@ class Canvas():
         self.imgArray = np.zeros((self.sizex, self.sizey, 3), np.uint8)
         self.imgFile = Image.fromarray(self.imgArray)
         self.data = self.imgFile.getdata()
+        self.bands = self.imgFile.getbands()
 
     def save(self):
         cv.imwrite(sys.argv[2], self.imgArray)
@@ -37,6 +39,9 @@ class TestCanvas(Canvas):
         self.sizex = Canvas.sizex
         self.sizey = Canvas.sizey
         self.imgArray = Canvas.imgArray
+        self.imgFile = Canvas.imgFile
+        self.data = Canvas.data
+        self.bands = Canvas.bands
 
     def addShape(self, shape):
         if shape == "line":
@@ -48,11 +53,34 @@ class TestCanvas(Canvas):
     def save(self):
         cv.imwrite(sys.argv[2], self.imgArray)
 
-def compare(a, b):
-    pairs = izip(a, b)
-    if len(Canvas.imgFile.getbands()) == 1:
+
+def compare(Image1, Image2):
+    #called like compare(canvas.data, source.data, canvas.bands)
+    pairs = izip(Image1.data, Image2.data)
+    if len(Image1.bands) == 1:
         dif = sum(abs(p1-p2) for p1,p2 in pairs)
     else:
-        dif = sum(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2)
+        dif = sum(abs(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2))
+        ncomponents = Image1.imgFile.size[0] * Image2.imgFile.size[1] * 3
+        return (dif / 255.0 * 100) / ncomponents
 
-        ncomponents = Canvas.imgFile.size[0] * self.imgFile
+
+source = SourceImg(sys.argv[1])
+print "source.sizex: ", source.sizex
+print "source.sizey: ", source.sizey
+
+canvas = Canvas(source)
+print "canvas.sizex: ", canvas.sizex
+print "canvas.sizey: ", canvas.sizey
+
+test = TestCanvas(canvas)
+print "test.sizex: ", test.sizex
+print "test.sizey: ", test.sizey
+
+print compare(source, canvas)
+print compare(canvas, test)
+
+test.addShape("line")
+test.save()
+print compare(canvas, test)
+
