50f35953e953dad1da8dd605686592fbdc96b475
Graham Hough <graham@hough.io>
2017-10-27 16:59:43 -0400

    getting there...

--- v2.py
+++ v2.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import random
 from itertools import izip
 from PIL import Image
@@ -17,8 +18,9 @@ class SourceImg():
         self.sizey =self.imgFile.size[1]
         self.data = self.imgFile.getdata()
         self.bands = self.imgFile.getbands()
+        self.diff = float
 
-class Canvas():
+class Canvas(SourceImg):
     """ A blank, black canvas """
 
     def __init__(self, SourceImg):
@@ -28,42 +30,39 @@ class Canvas():
         self.imgFile = Image.fromarray(self.imgArray)
         self.data = self.imgFile.getdata()
         self.bands = self.imgFile.getbands()
+        self.diff = 99.0
 
-    def save(self):
-        cv.imwrite(sys.argv[2], self.imgArray)
-
-class TestCanvas(Canvas):
-    """ An intermediate canvas for data processing """
-
-    def __init__(self, Canvas):
-        self.sizex = Canvas.sizex
-        self.sizey = Canvas.sizey
-        self.imgArray = Canvas.imgArray
-        self.imgFile = Canvas.imgFile
-        self.data = Canvas.data
-        self.bands = Canvas.bands
 
     def addShape(self, shape):
         if shape == "line":
             self.line()
+        if shape == "rectangle":
+            self.rectangle()
 
     def line(self):
         self.imgArray = cv.line(self.imgArray,(random.randrange(self.sizex),random.randrange(self.sizey)),(random.randrange(self.sizex),random.randrange(self.sizey)),(random.randrange(256),random.randrange(256),random.randrange(256)),1)
 
-    def save(self):
+    def rectangle(self):
+        self.imgArray = cv.rectangle(self.imgArray,(random.randrange(self.sizex),random.randrange(self.sizey)),(random.randrange(self.sizex),random.randrange(self.sizey)),(random.randrange(256),random.randrange(256),random.randrange(256)),1)
+
+    #added name for testing
+    def save(self, name):
+        if name:
+          cv.imwrite(name, self.imgArray)
         cv.imwrite(sys.argv[2], self.imgArray)
 
-
 def compare(Image1, Image2):
-    #called like compare(canvas.data, source.data, canvas.bands)
+    Image2.imgFile = Image.fromarray(Image2.imgArray)
+    Image2.data = Image2.imgFile.getdata()
+    Image2.bands = Image2.imgFile.getbands()
     pairs = izip(Image1.data, Image2.data)
     if len(Image1.bands) == 1:
         dif = sum(abs(p1-p2) for p1,p2 in pairs)
     else:
         dif = sum(abs(c1-c2) for p1,p2 in pairs for c1,c2 in zip(p1,p2))
         ncomponents = Image1.imgFile.size[0] * Image2.imgFile.size[1] * 3
+        Image2.diff = (dif / 255.0 * 100) / ncomponents
         return (dif / 255.0 * 100) / ncomponents
-
 
 source = SourceImg(sys.argv[1])
 print "source.sizex: ", source.sizex
@@ -73,14 +72,37 @@ canvas = Canvas(source)
 print "canvas.sizex: ", canvas.sizex
 print "canvas.sizey: ", canvas.sizey
 
-test = TestCanvas(canvas)
+test = Canvas(canvas)
 print "test.sizex: ", test.sizex
 print "test.sizey: ", test.sizey
 
-print compare(source, canvas)
-print compare(canvas, test)
+compare(source, canvas)
+print canvas.diff
+print source.diff
 
-test.addShape("line")
-test.save()
-print compare(canvas, test)
+yes = 0
+no = 0
+count = 0
 
+while source.diff > 20:
+  test.addShape("line")
+  compare(source, test)
+
+  if test.diff < canvas.diff:
+    print  canvas.diff, test.diff, "yes: ", yes, "no: ", no
+    canvas.imgArray = test.imgArray
+    canvas.data = test.data
+    canvas.imgFile = Image.fromarray(canvas.imgArray)
+    canvas.data = canvas.imgFile.getdata()
+    compare(source, canvas)
+    yes += 1
+    canvas.save(str(count) + ".jpg")
+  else:
+    print  canvas.diff, test.diff, "yes: ", yes, "no: ", no
+    test.imgArray = []
+    test.imgArray = canvas.imgArray
+    test.imgFile = Image.fromarray(canvas.imgArray)
+    compare(source, test)
+    no += 1
+  count += 1
+canvas.save()
